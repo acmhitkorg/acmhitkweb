747a9f2477c9f7800b55968f0e61c46372444e8d
rohits-web03 <rohit.dey302003@gmail.com>
2025-12-22 21:07:41 +0530

    disable image optimization for static export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isStatic = process.env.NEXT_PUBLIC_STATIC_EXPORT === 'true'
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+	output: isStatic ? 'export' : undefined,
+	trailingSlash: true,
+	images: {
+		unoptimized: isStatic
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
