6c1df37e18d75b2b6e76e47fb520640b9567f8c8
rohits-web03 <rohit.dey302003@gmail.com>
2025-12-22 20:54:41 +0530

    Add images for welcome.yml in webp format

--- data/gallery-data.ts
+++ data/gallery-data.ts
@@ -1,194 +1,223 @@
 export const gallerySections = [
-    {
-        year: "2025-26",
-        events: [
-            {
-                date: "August 2025",
-                name: "Program Paradox",
-                path: "/ACM-Pics-2025_26/Program-Paradox/webp",
-                images: [
-                    "Program-Paradox-0.webp",
-                    "Program-Paradox-1.webp",
-                    "Program-Paradox-2.webp",
-                    "Program-Paradox-3.webp",
-                    "Program-Paradox-4.webp",
-                    "Program-Paradox-5.webp",
-                    "Program-Paradox-6.webp",
-                    "Program-Paradox-7.webp",
-                    "Program-Paradox-8.webp",
-                    "Program-Paradox-9.webp",
-                    "Program-Paradox-10.webp",
-                    "Program-Paradox-11.webp",
-                    "Program-Paradox-12.webp",
-                    "Program-Paradox-13.webp",
-                ]
-            },
-            {
-                date: "September 2025",
-                name: "Put On Your Computing Cap",
-                path: "/ACM-Pics-2025_26/Put-On-Your-Computing-Cap/webp",
-                images: [
-                    "Put-On-Your-Computing-Cap-0.webp",
-                    "Put-On-Your-Computing-Cap-1.webp",
-                    "Put-On-Your-Computing-Cap-2.webp",
-                    "Put-On-Your-Computing-Cap-3.webp",
-                    "Put-On-Your-Computing-Cap-4.webp",
-                    "Put-On-Your-Computing-Cap-5.webp",
-                    "Put-On-Your-Computing-Cap-6.webp",
-                    "Put-On-Your-Computing-Cap-7.webp",
-                    "Put-On-Your-Computing-Cap-8.webp",
-                    "Put-On-Your-Computing-Cap-9.webp",
-                    "Put-On-Your-Computing-Cap-10.webp",
-                    "Put-On-Your-Computing-Cap-11.webp",
-                    "Put-On-Your-Computing-Cap-12.webp",
-                    "Put-On-Your-Computing-Cap-13.webp",
-                    "Put-On-Your-Computing-Cap-14.webp",
-                    "Put-On-Your-Computing-Cap-15.webp",
-                    "Put-On-Your-Computing-Cap-16.webp",
-                    "Put-On-Your-Computing-Cap-17.webp",
-                    "Put-On-Your-Computing-Cap-18.webp",
-                    "Put-On-Your-Computing-Cap-19.webp",
-                    "Put-On-Your-Computing-Cap-20.webp",
-                    "Put-On-Your-Computing-Cap-21.webp",
-                    "Put-On-Your-Computing-Cap-22.webp",
-                ]
-            }
-        ]
-    },
-    {
-        year: "2024-25",
-        events: [
-            {
-                date: "March 2025",
-                name: "Brainstorm Session",
-                path: "/ACM-Pics-2024_25/Geo-Tagged-Pic/10th March BrainStorm_25",
-                images: [
-                    "20250310_41311PM_ByGPSMapCamera.jpg",
-                    "20250310_41327PM_ByGPSMapCamera.jpg",
-                    "20250310_41331PM_ByGPSMapCamera.jpg",
-                    "20250310_41508pm_ByGPSMapCamera.jpg",
-                    "20250310_41511pm_ByGPSMapCamera.jpg",
-                    "20250310_41517pm_ByGPSMapCamera.jpg",
-                    "20250310_41520pm_ByGPSMapCamera.jpg",
-                    "20250310_41527pm_ByGPSMapCamera.jpg",
-                    "20250310_41534pm_ByGPSMapCamera.jpg",
-                    "20250310_41553pm_ByGPSMapCamera.jpg",
-                    "20250310_41556pm_ByGPSMapCamera.jpg",
-                    "20250310_41620pm_ByGPSMapCamera.jpg",
-                    "20250310_41622pm_ByGPSMapCamera.jpg",
-                    "20250310_41629pm_ByGPSMapCamera.jpg"
-                ]
-            },
-            {
-                date: "November 2024",
-                name: "ACM Annual Regional Meet",
-                path: "/ACM-Pics-2024_25/ACM ARM",
-                images: [
-                    "arm-img3.jpg",
-                    "arm-img2.jpg",
-                    "arm-img1.jpg",
-                    "arm-img0.jpg",
-                ]
-            }
-        ]
-    },
-    {
-        year: "2023-24",
-        events: [
-            {
-                date: "May 2024",
-                name: "Tanmoy Chakraborty Session",
-                path: "/ACM-Pics-2023_24/Tanmoy Chakraborty talk on LLMs",
-                images: ["Tanmoy-Chakraborty-talk.JPG",
-                    "Tanmoy-Chakraborty-talk-1.JPG"]
-            },
-            {
-                date: "April 2024",
-                name: "Generative AI Workshop",
-                path: "/ACM-Pics-2023_24/16_April_2024",
-                images: [
-                    "Generative AI Pic1.jpeg",
-                    "Generative AI Pic2.jpeg"
-                ]
-            },
-            {
-                date: "April 2024",
-                name: "Sourav Sir Session",
-                path: "/ACM-Pics-2023_24/15_April_2024",
-                images: [
-                    "Sourav Sir ACM 1.jpg",
-                    "Sourav Sir ACM 2.jpg",
-                    "Sourav Sir ACM 3.jpg",
-                    "Sourav Sir ACM 4.jpg",
-                    "Sourav Sir Presentation.jpeg",
-                    "Sourav Sir Presentation1.jpeg",
-                    "Sourav Sir Presentation2.jpeg"
-                ]
-            },
-            {
-                date: "February 2024",
-                name: "Brainstorm Session",
-                path: "/ACM-Pics-2023_24/26_Feb_2024 Brainstorm",
-                images: [
-                    "1.jpg",
-                    "2.jpg",
-                    "IMG-20240226-WA0003.jpg",
-                    "IMG-20240226-WA0005.jpg",
-                    "IMG-20240226-WA0006.jpg",
-                    "IMG-20240226-WA0007.jpg",
-                    "IMG-20240226-WA0008.jpg"
-                ]
-            },
-            {
-                date: "February 2024",
-                name: "TIFR Visit",
-                path: "/ACM-Pics-2023_24/09-Feb-2024",
-                images: [
-                    "09-02-2024_TIFR_1.jpeg",
-                    "09-02-2024_TIFR_2.jpeg",
-                    "09-02-2024_TIFR_3.jpeg"
-                ]
-            },
-            {
-                date: "December 2023",
-                name: "Michael Chen Talk",
-                path: "/ACM-Pics-2023_24/14th Dec_2023_ Michael Chen",
-                images: [
-                    "Michael_s Talk_1.jpg",
-                    "Michael_s Talk_2.jpg",
-                    "Michael_s Talk_3.jpg"
-                ]
-            },
-            {
-                date: "September 2023",
-                name: "Ashmi Banerjee Session",
-                path: "/ACM-Pics-2023_24/27-SeP Ashmi",
-                images: [
-                    "20230927_41347PMByGPSMapCamera.jpg",
-                    "20230927_41353PMByGPSMapCamera.jpg",
-                    "20230927_41405PMByGPSMapCamera.jpg"
-                ]
-            }
-        ]
-    },
-    {
-        year: "2022-23",
-        events: [
-            {
-                date: "2022-23",
-                name: "ACM Events",
-                path: "/ACM-Pics-2022_23",
-                images: [
-                    "10_02_2023.jpg",
-                    "1666162499885.jpg",
-                    "20220916_25154PMByGPSMapCamera.jpg",
-                    "20230310_30414pmByGPSMapCamera.jpg",
-                    "20230310_33558pmByGPSMapCamera.jpg",
-                    "504acc41-ae28-469c-b001-e366cbc1227a.jpg",
-                    "6d872639-0099-4d47-8079-82ddfd0c3097.jpg",
-                    "ed4cf317-214f-42cf-b14f-a47003c5c29e.jpg"
-                ]
-            }
-        ]
-    }
+	{
+		year: "2025-26",
+		events: [
+			{
+				date: "November 2025",
+				name: "Welcome.yml",
+				path: "/ACM-Pics-2025_26/welcome-yml/webp",
+				images: [
+					"1.webp",
+					"2.webp",
+					"3.webp",
+					"4.webp",
+					"5.webp",
+					"6.webp",
+					"7.webp",
+					"8.webp",
+					"9.webp",
+					"10.webp",
+					"11.webp",
+					"12.webp",
+					"13.webp",
+					"14.webp",
+					"15.webp",
+					"16.webp",
+					"17.webp",
+					"18.webp",
+					"19.webp",
+					"20.webp",
+					"21.webp",
+					"22.webp"
+				]
+			},
+			{
+				date: "August 2025",
+				name: "Program Paradox",
+				path: "/ACM-Pics-2025_26/Program-Paradox/webp",
+				images: [
+					"Program-Paradox-0.webp",
+					"Program-Paradox-1.webp",
+					"Program-Paradox-2.webp",
+					"Program-Paradox-3.webp",
+					"Program-Paradox-4.webp",
+					"Program-Paradox-5.webp",
+					"Program-Paradox-6.webp",
+					"Program-Paradox-7.webp",
+					"Program-Paradox-8.webp",
+					"Program-Paradox-9.webp",
+					"Program-Paradox-10.webp",
+					"Program-Paradox-11.webp",
+					"Program-Paradox-12.webp",
+					"Program-Paradox-13.webp",
+				]
+			},
+			{
+				date: "September 2025",
+				name: "Put On Your Computing Cap",
+				path: "/ACM-Pics-2025_26/Put-On-Your-Computing-Cap/webp",
+				images: [
+					"Put-On-Your-Computing-Cap-0.webp",
+					"Put-On-Your-Computing-Cap-1.webp",
+					"Put-On-Your-Computing-Cap-2.webp",
+					"Put-On-Your-Computing-Cap-3.webp",
+					"Put-On-Your-Computing-Cap-4.webp",
+					"Put-On-Your-Computing-Cap-5.webp",
+					"Put-On-Your-Computing-Cap-6.webp",
+					"Put-On-Your-Computing-Cap-7.webp",
+					"Put-On-Your-Computing-Cap-8.webp",
+					"Put-On-Your-Computing-Cap-9.webp",
+					"Put-On-Your-Computing-Cap-10.webp",
+					"Put-On-Your-Computing-Cap-11.webp",
+					"Put-On-Your-Computing-Cap-12.webp",
+					"Put-On-Your-Computing-Cap-13.webp",
+					"Put-On-Your-Computing-Cap-14.webp",
+					"Put-On-Your-Computing-Cap-15.webp",
+					"Put-On-Your-Computing-Cap-16.webp",
+					"Put-On-Your-Computing-Cap-17.webp",
+					"Put-On-Your-Computing-Cap-18.webp",
+					"Put-On-Your-Computing-Cap-19.webp",
+					"Put-On-Your-Computing-Cap-20.webp",
+					"Put-On-Your-Computing-Cap-21.webp",
+					"Put-On-Your-Computing-Cap-22.webp",
+				]
+			}
+		]
+	},
+	{
+		year: "2024-25",
+		events: [
+			{
+				date: "March 2025",
+				name: "Brainstorm Session",
+				path: "/ACM-Pics-2024_25/Geo-Tagged-Pic/10th March BrainStorm_25",
+				images: [
+					"20250310_41311PM_ByGPSMapCamera.jpg",
+					"20250310_41327PM_ByGPSMapCamera.jpg",
+					"20250310_41331PM_ByGPSMapCamera.jpg",
+					"20250310_41508pm_ByGPSMapCamera.jpg",
+					"20250310_41511pm_ByGPSMapCamera.jpg",
+					"20250310_41517pm_ByGPSMapCamera.jpg",
+					"20250310_41520pm_ByGPSMapCamera.jpg",
+					"20250310_41527pm_ByGPSMapCamera.jpg",
+					"20250310_41534pm_ByGPSMapCamera.jpg",
+					"20250310_41553pm_ByGPSMapCamera.jpg",
+					"20250310_41556pm_ByGPSMapCamera.jpg",
+					"20250310_41620pm_ByGPSMapCamera.jpg",
+					"20250310_41622pm_ByGPSMapCamera.jpg",
+					"20250310_41629pm_ByGPSMapCamera.jpg"
+				]
+			},
+			{
+				date: "November 2024",
+				name: "ACM Annual Regional Meet",
+				path: "/ACM-Pics-2024_25/ACM ARM",
+				images: [
+					"arm-img3.jpg",
+					"arm-img2.jpg",
+					"arm-img1.jpg",
+					"arm-img0.jpg",
+				]
+			}
+		]
+	},
+	{
+		year: "2023-24",
+		events: [
+			{
+				date: "May 2024",
+				name: "Tanmoy Chakraborty Session",
+				path: "/ACM-Pics-2023_24/Tanmoy Chakraborty talk on LLMs",
+				images: ["Tanmoy-Chakraborty-talk.JPG",
+					"Tanmoy-Chakraborty-talk-1.JPG"]
+			},
+			{
+				date: "April 2024",
+				name: "Generative AI Workshop",
+				path: "/ACM-Pics-2023_24/16_April_2024",
+				images: [
+					"Generative AI Pic1.jpeg",
+					"Generative AI Pic2.jpeg"
+				]
+			},
+			{
+				date: "April 2024",
+				name: "Sourav Sir Session",
+				path: "/ACM-Pics-2023_24/15_April_2024",
+				images: [
+					"Sourav Sir ACM 1.jpg",
+					"Sourav Sir ACM 2.jpg",
+					"Sourav Sir ACM 3.jpg",
+					"Sourav Sir ACM 4.jpg",
+					"Sourav Sir Presentation.jpeg",
+					"Sourav Sir Presentation1.jpeg",
+					"Sourav Sir Presentation2.jpeg"
+				]
+			},
+			{
+				date: "February 2024",
+				name: "Brainstorm Session",
+				path: "/ACM-Pics-2023_24/26_Feb_2024 Brainstorm",
+				images: [
+					"1.jpg",
+					"2.jpg",
+					"IMG-20240226-WA0003.jpg",
+					"IMG-20240226-WA0005.jpg",
+					"IMG-20240226-WA0006.jpg",
+					"IMG-20240226-WA0007.jpg",
+					"IMG-20240226-WA0008.jpg"
+				]
+			},
+			{
+				date: "February 2024",
+				name: "TIFR Visit",
+				path: "/ACM-Pics-2023_24/09-Feb-2024",
+				images: [
+					"09-02-2024_TIFR_1.jpeg",
+					"09-02-2024_TIFR_2.jpeg",
+					"09-02-2024_TIFR_3.jpeg"
+				]
+			},
+			{
+				date: "December 2023",
+				name: "Michael Chen Talk",
+				path: "/ACM-Pics-2023_24/14th Dec_2023_ Michael Chen",
+				images: [
+					"Michael_s Talk_1.jpg",
+					"Michael_s Talk_2.jpg",
+					"Michael_s Talk_3.jpg"
+				]
+			},
+			{
+				date: "September 2023",
+				name: "Ashmi Banerjee Session",
+				path: "/ACM-Pics-2023_24/27-SeP Ashmi",
+				images: [
+					"20230927_41347PMByGPSMapCamera.jpg",
+					"20230927_41353PMByGPSMapCamera.jpg",
+					"20230927_41405PMByGPSMapCamera.jpg"
+				]
+			}
+		]
+	},
+	{
+		year: "2022-23",
+		events: [
+			{
+				date: "2022-23",
+				name: "ACM Events",
+				path: "/ACM-Pics-2022_23",
+				images: [
+					"10_02_2023.jpg",
+					"1666162499885.jpg",
+					"20220916_25154PMByGPSMapCamera.jpg",
+					"20230310_30414pmByGPSMapCamera.jpg",
+					"20230310_33558pmByGPSMapCamera.jpg",
+					"504acc41-ae28-469c-b001-e366cbc1227a.jpg",
+					"6d872639-0099-4d47-8079-82ddfd0c3097.jpg",
+					"ed4cf317-214f-42cf-b14f-a47003c5c29e.jpg"
+				]
+			}
+		]
+	}
 ]
