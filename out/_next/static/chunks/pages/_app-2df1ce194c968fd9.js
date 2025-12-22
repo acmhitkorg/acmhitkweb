146703453290b956323c287788891ea660608975
rohits-web03 <rohit.dey302003@gmail.com>
2025-12-23 02:34:53 +0530

    update build output

---
+++ out/_next/static/chunks/pages/_app-2df1ce194c968fd9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{611:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(1068)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(611),n(9898))),_N_E=_.O()}]);
