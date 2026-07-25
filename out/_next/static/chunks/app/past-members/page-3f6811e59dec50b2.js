145e8ba48c70cb069b9579369adc8b85de4c2091
Srijit Roy <rsrijit436@gmail.com>
2026-07-25 18:15:05 +0530

    Build changes

---
+++ out/_next/static/chunks/app/past-members/page-3f6811e59dec50b2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[777],{5766:(e,s,n)=>{Promise.resolve().then(n.bind(n,3153)),Promise.resolve().then(n.bind(n,1015)),Promise.resolve().then(n.bind(n,9624))}},e=>{e.O(0,[644,422,716,624,441,964,358],()=>e(e.s=5766)),_N_E=e.O()}]);
